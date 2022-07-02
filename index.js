c3b85548440399a5795270fcddaf52d0d6344862
Sumit <studyzone.sumit@gmail.com>
2022-07-02 15:14:32 +0530

    First Commit: set up express server and directory structure

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port, (err)=>{
+    if(err){
+        console.log(`Error: ${err}`);
+        return;
+    }
+    console.log(`Server is up! And running on port: ${port}`);
+});
