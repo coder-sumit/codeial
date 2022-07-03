b4e86cfbe99fc3ec5bb133b2c997f4a361a8dedf
Sumit <studyzone.sumit@gmail.com>
2022-07-03 15:13:22 +0530

    add partials and layouts

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(expressLayouts);
 // use express router
 app.use('/', require('./routes/index'));
 
